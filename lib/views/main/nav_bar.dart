29459d6b2758e74204c6c1e7bcbcd8511d8ed51f
Yong Yang <55826849+boonyongyang@users.noreply.github.com>
2023-01-30 22:38:19 +0800

    remove drawer

--- lib/views/main/nav_bar.dart
+++ lib/views/main/nav_bar.dart
@@ -39,43 +39,6 @@ class _MaterialYouState extends State<MaterialYou> {
       // appBar: AppBar(
       //   title: const Text(''),
       // ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              child: Text(
-                'Drawer Header',
-              ),
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-            ),
-            ListTile(
-              title: Text(
-                'Item 1',
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Item 2',
-              ),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
       body: Center(
         child: pages[_currentIndex],
       ),
