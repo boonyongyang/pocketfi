4d5280ff5e051fcdf03500234c2f8a2d3903b901
Annebel Choong <annebelchoong@gmail.com>
2023-02-19 14:32:14 +0800

    Add provider that view all the wallet in firebase

---
+++ lib/state/tabs/budget/wallet/provider/user_wallets_provider.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pocketfi/state/auth/providers/user_id_provider.dart';
+import 'package:pocketfi/state/constants/firebase_collection_name.dart';
+import 'package:pocketfi/state/constants/firebase_field_name.dart';
+import 'package:pocketfi/state/tabs/budget/wallet/models/wallet.dart';
+
+final userWalletsProvider = StreamProvider.autoDispose<Iterable<Wallet>>((ref) {
+  final userId = ref.watch(userIdProvider);
+  final controller = StreamController<Iterable<Wallet>>();
+
+  final sub = FirebaseFirestore.instance
+      .collection(FirebaseCollectionName.wallets)
+      .where(FirebaseFieldName.userId, isEqualTo: userId)
+      .snapshots()
+      .listen((snapshot) {
+    final document = snapshot.docs;
+    final wallets = document.where((doc) => !doc.metadata.hasPendingWrites).map(
+          (doc) => Wallet(walletId: doc.id, doc.data()),
+        );
+    controller.sink.add(wallets);
+  });
+
+  ref.onDispose(() {
+    sub.cancel();
+    controller.close();
+  });
+  return controller.stream;
+});
