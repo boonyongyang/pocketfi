a4c1d2dd8cf3db4bd374cca1a903d4185b849de3
Annebel Choong <annebelchoong@gmail.com>
2023-02-24 20:05:41 +0800

    Change to flex

--- lib/views/tabs/budget/budget_page.dart
+++ lib/views/tabs/budget/budget_page.dart
@@ -44,112 +44,132 @@ class _BudgetPageState extends ConsumerState<BudgetPage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          // return await ref.refresh(userWalletsProvider);
+          // return Future.delayed(const Duration(seconds: 1));
+        },
+        child: Flex(
+          direction: Axis.vertical,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
+            Expanded(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Row(
-                    children: const [
-                      Text(
-                        'Total',
-                        style: TextStyle(
-                          color: AppSwatches.mainColor1,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              'Total',
+                              style: TextStyle(
+                                color: AppSwatches.mainColor1,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Spacer(),
+                            // Calculation part
+                            Text(
+                              'RM XX.XX',
+                              style: TextStyle(
+                                color: AppSwatches.mainColor1,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Spacer(),
-                      // Calculation part
-                      Text(
-                        'RM XX.XX',
-                        style: TextStyle(
-                          color: AppSwatches.mainColor1,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                        const SizedBox(
+                          height: 10,
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const [
-                      // Calculation part
-                      Text(
-                        'RM XX.XX left',
-                        style: TextStyle(
-                          color: AppSwatches.mainColor2,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: const [
+                            // Calculation part
+                            Text(
+                              'RM XX.XX left',
+                              style: TextStyle(
+                                color: AppSwatches.mainColor2,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
+                  const Divider(),
+                  BudgetTile(
+                    budget: 12.00,
+                    categoryName: CategorySetting.foodAndDrink.name,
+                    categoryIcon: CategorySetting.foodAndDrink.icons,
+                    categoryColor: CategorySetting.foodAndDrink.color,
+                  ),
+                  // BudgetTile(
+                  //   budget: 12.00,
+                  //   categoryName: CategorySetting.groceries.name,
+                  //   categoryIcon: CategorySetting.groceries.icons,
+                  //   categoryColor: CategorySetting.groceries.color,
+                  // ),
+                  // BudgetTile(
+                  //   budget: 12.00,
+                  //   categoryName: CategorySetting.shopping.name,
+                  //   categoryIcon: CategorySetting.shopping.icons,
+                  //   categoryColor: CategorySetting.shopping.color,
+                  // ),
+                  // BudgetTile(
+                  //   budget: 12.00,
+                  //   categoryName: CategorySetting.beauty.name,
+                  //   categoryIcon: CategorySetting.beauty.icons,
+                  //   categoryColor: CategorySetting.beauty.color,
+                  // ),
+                  // BudgetTile(
+                  //   budget: 12.00,
+                  //   categoryName: CategorySetting.billsAndFees.name,
+                  //   categoryIcon: CategorySetting.billsAndFees.icons,
+                  //   categoryColor: CategorySetting.billsAndFees.color,
+                  // ),
                 ],
               ),
             ),
-            const Divider(),
-            BudgetTile(
-              budget: 12.00,
-              categoryName: CategorySetting.foodAndDrink.name,
-              categoryIcon: CategorySetting.foodAndDrink.icons,
-              categoryColor: CategorySetting.foodAndDrink.color,
-            ),
-            // BudgetTile(
-            //   budget: 12.00,
-            //   categoryName: CategorySetting.groceries.name,
-            //   categoryIcon: CategorySetting.groceries.icons,
-            //   categoryColor: CategorySetting.groceries.color,
-            // ),
-            // BudgetTile(
-            //   budget: 12.00,
-            //   categoryName: CategorySetting.shopping.name,
-            //   categoryIcon: CategorySetting.shopping.icons,
-            //   categoryColor: CategorySetting.shopping.color,
-            // ),
-            // BudgetTile(
-            //   budget: 12.00,
-            //   categoryName: CategorySetting.beauty.name,
-            //   categoryIcon: CategorySetting.beauty.icons,
-            //   categoryColor: CategorySetting.beauty.color,
-            // ),
-            // BudgetTile(
-            //   budget: 12.00,
-            //   categoryName: CategorySetting.billsAndFees.name,
-            //   categoryIcon: CategorySetting.billsAndFees.icons,
-            //   categoryColor: CategorySetting.billsAndFees.color,
-            // ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(80, 55),
-                  backgroundColor: AppSwatches.mainColor1,
-                  foregroundColor: AppSwatches.white,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(25),
+            Expanded(
+              flex: 0,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(80, 55),
+                        backgroundColor: AppSwatches.mainColor1,
+                        foregroundColor: AppSwatches.white,
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+                      ),
+                      onPressed: () =>
+                          // {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (_) => const CreateNewBudgetView(),
+                          //   ),
+                          // );
+                          context.beamToNamed("createNewBudget"),
+                      // },
+                      child: const ButtonWidget(
+                        text: Strings.createNewBudget,
+                      ),
                     ),
                   ),
-                ),
-                onPressed: () =>
-                    // {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (_) => const CreateNewBudgetView(),
-                    //   ),
-                    // );
-                    context.beamToNamed("createNewBudget"),
-                // },
-                child: const ButtonWidget(
-                  text: Strings.createNewBudget,
                 ),
               ),
             ),
