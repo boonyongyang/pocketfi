1cc37b67f63438984c0b8c639f0563bedb02c296
Annebel Choong <annebelchoong@gmail.com>
2023-02-24 12:44:54 +0800

    Change ui

--- lib/views/tabs/budget/wallet/create_new_wallet_view.dart
+++ lib/views/tabs/budget/wallet/create_new_wallet_view.dart
@@ -60,85 +60,212 @@ class _CreateNewWalletViewState extends ConsumerState<CreateNewWalletView> {
       appBar: AppBar(
         title: const Text(Strings.createNewWallet),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Flex(
+        direction: Axis.vertical,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: walletNameController,
-              decoration: const InputDecoration(
-                labelText: Strings.walletName,
-              ),
-              // autofocus: true,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: initialBalanceController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: Strings.walletBalance,
-              ),
-            ),
-          ),
-          // Expanded(
-          //   flex: 1,
-          //   child: Align(
-          //     alignment: Alignment.bottomCenter,
-          //     child:
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(80, 55),
-                backgroundColor: AppSwatches.mainColor1,
-                foregroundColor: AppSwatches.white,
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25),
+          Expanded(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 32.0),
+                      child: SizedBox(
+                        width: 5,
+                        child: Icon(
+                          Icons.wallet,
+                          color: AppSwatches.mainColor1,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: walletNameController,
+                          decoration: const InputDecoration(
+                            labelText: Strings.walletName,
+                          ),
+                          autofocus: true,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 32.0),
+                      child: SizedBox(
+                        width: 5,
+                        child: Icon(
+                          Icons.money_rounded,
+                          color: AppSwatches.mainColor1,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: initialBalanceController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            labelText: Strings.walletBalance,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 32.0),
+                      child: SizedBox(
+                        width: 5,
+                        child: Icon(
+                          Icons.category_rounded,
+                          color: AppSwatches.mainColor1,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Expanded(
+                              child: Text(
+                                'Visible Categories',
+                                style: TextStyle(
+                                  color: AppSwatches.mainColor1,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'XX',
+                              style: TextStyle(
+                                color: AppSwatches.mainColor1,
+                                fontSize: 16,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                color: AppSwatches.mainColor1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0, right: 32.0),
+                      child: SizedBox(
+                        width: 5,
+                        child: Icon(
+                          Icons.people_alt_rounded,
+                          color: AppSwatches.mainColor1,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: const [
+                            Expanded(
+                              child: Text(
+                                'Share Wallet with Other People',
+                                style: TextStyle(
+                                  color: AppSwatches.mainColor1,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                color: AppSwatches.mainColor1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(80, 55),
+                            backgroundColor: AppSwatches.mainColor1,
+                            foregroundColor: AppSwatches.white,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(25),
+                              ),
+                            ),
+                          ),
+                          onPressed: isCreateButtonEnabled.value
+                              ? () async {
+                                  _createNewWalletController(
+                                    walletNameController,
+                                    initialBalanceController,
+                                    ref,
+                                  );
+                                  // final userId = ref.read(
+                                  //   userIdProvider,
+                                  // );
+                                  // if (userId == null) {
+                                  //   return;
+                                  // }
+                                  // final message = walletNameController.text;
+                                  // final amount = initialBalanceController.text;
+                                  // // hook the UI to the imageUploadProvider for uploading the post
+                                  // // hooking the UI to the provider will cause the UI to rebuild
+                                  // final isUploaded = await ref
+                                  //     .read(createNewWalletProvider.notifier)
+                                  //     .createNewWallet(
+                                  //       userId: userId,
+                                  //       walletName: message,
+                                  //       walletBalance: double.parse(amount),
+                                  //     );
+                                  // if (isUploaded && mounted) {
+                                  //   // if the post is uploaded, then pop the screen
+                                  //   // Navigator.of(context).pop();
+                                  //   Beamer.of(context).beamBack();
+                                  // }
+                                }
+                              : null,
+                          child: const ButtonWidget(
+                            text: Strings.createNewWallet,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              onPressed: isCreateButtonEnabled.value
-                  ? () async {
-                      _createNewWalletController(
-                        walletNameController,
-                        initialBalanceController,
-                        ref,
-                      );
-                      // final userId = ref.read(
-                      //   userIdProvider,
-                      // );
-                      // if (userId == null) {
-                      //   return;
-                      // }
-                      // final message = walletNameController.text;
-                      // final amount = initialBalanceController.text;
-                      // // hook the UI to the imageUploadProvider for uploading the post
-                      // // hooking the UI to the provider will cause the UI to rebuild
-                      // final isUploaded = await ref
-                      //     .read(createNewWalletProvider.notifier)
-                      //     .createNewWallet(
-                      //       userId: userId,
-                      //       walletName: message,
-                      //       walletBalance: double.parse(amount),
-                      //     );
-                      // if (isUploaded && mounted) {
-                      //   // if the post is uploaded, then pop the screen
-                      //   // Navigator.of(context).pop();
-                      //   Beamer.of(context).beamBack();
-                      // }
-                    }
-                  : null,
-              child: const ButtonWidget(
-                text: Strings.createNewWallet,
-              ),
+              ],
             ),
           ),
-          // ),
-          // ),
         ],
       ),
     );
