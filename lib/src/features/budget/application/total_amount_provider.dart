6f6c4df1f7454e9c2e119772361f24d813af7814
Annebel Choong <annebelchoong@gmail.com>
2023-02-28 00:53:04 +0800

    add provider to get all budget amount and add tgt

---
+++ lib/src/features/budget/application/total_amount_provider.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pocketfi/src/constants/firebase_collection_name.dart';
+import 'package:pocketfi/src/features/authentication/application/user_id_provider.dart';
+import 'package:pocketfi/src/features/budget/domain/budget.dart';
+
+final totalAmountProvider = StreamProvider.autoDispose<double>((ref) {
+  final userId = ref.watch(userIdProvider);
+  // !temporary only
+  const walletId = '2023-02-27T21:08:26.256268';
+  final controller = StreamController<double>();
+
+  final sub = FirebaseFirestore.instance
+      .collection(FirebaseCollectionName.users)
+      .doc(userId)
+      .collection(FirebaseCollectionName.wallets)
+      .doc(walletId)
+      .collection(FirebaseCollectionName.budgets)
+      .snapshots()
+      .listen((snapshot) {
+    final document = snapshot.docs;
+    final budgets = document.map(
+      (doc) => Budget(doc.data()).budgetAmount,
+    );
+    // get budgetAmount from all budgets code
+    final totalBudgetAmount = budgets.fold(
+      0.00,
+      (previousValue, element) => previousValue + element,
+    );
+
+    controller.sink.add(totalBudgetAmount);
+  });
+
+  ref.onDispose(() {
+    sub.cancel();
+    controller.close();
+  });
+  return controller.stream;
+});
