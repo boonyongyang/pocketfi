3ed600edde6e282a42507101d010a920c9a61199
Annebel Choong <annebelchoong@gmail.com>
2023-03-08 12:57:08 +0800

    add collaborators in wallet

--- lib/src/features/budget/wallet/domain/wallet.dart
+++ lib/src/features/budget/wallet/domain/wallet.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart' show immutable;
+
 import 'package:pocketfi/src/constants/firebase_field_name.dart';
 import 'package:pocketfi/src/constants/typedefs.dart';
+import 'package:pocketfi/src/features/authentication/domain/collaborators_info.dart';
+// import 'package:pocketfi/src/features/authentication/domain/user_info_model.dart';
 
 @immutable
 class Wallet {
@@ -11,6 +17,7 @@ class Wallet {
   final DateTime createdAt;
   // TODO maybe a list idk
   final UserId userId;
+  final List<dynamic>? collaborators;
   // TODO add final Category category;
 
   Wallet(Map<String, dynamic> json)
@@ -18,7 +25,8 @@ class Wallet {
         walletName = json[FirebaseFieldName.walletName],
         // walletBalance = json[FirebaseFieldName.walletBalance],
         createdAt = (json[FirebaseFieldName.createdAt] as Timestamp).toDate(),
-        userId = json[FirebaseFieldName.userId] as UserId;
+        userId = json[FirebaseFieldName.userId] as UserId,
+        collaborators = json[FirebaseFieldName.collaborators];
   // Wallet(Map<String, dynamic> json, {required this.walletId})
   //     : walletName = json[FirebaseFieldName.walletName],
   //       walletBalance = json[FirebaseFieldName.walletBalance],
@@ -32,6 +40,7 @@ class Wallet {
       other.walletName == walletName &&
       // other.walletBalance == walletBalance &&
       other.createdAt == createdAt &&
+      // other.collaborators == collaborators &&
       other.userId == userId;
 
   @override
